0075ee142cb37b76b66eb74c582f038ffdd55b26
bokjo <bstojcevski89@gmail.com>
2018-02-20 15:48:49 +0100

    Implemented and attached draft mobile layout for the Elderly Dogs Item...

---
+++ LearnSitecore/layouts/Dogs Alive/Mobile.aspx.cs
@@ -0,0 +1,13 @@
+namespace LearnSitecore.layouts.Dogs_Alive
+{
+    using System;
+    using System.Web;
+    using System.Web.UI;
+
+    public partial class Mobile : Page
+    {
+        private void Page_Load(object sender, System.EventArgs e)
+        {
+        }
+    }
+}
